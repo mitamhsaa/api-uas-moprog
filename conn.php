17c8057a8c9427bbe72974f95f483fd8afc1dcde
aftlah <altaf.fattah05@gmail.com>
2025-06-14 20:31:25 +0700

    [UPDATE]: configuration production

--- conn.php
+++ conn.php
@@ -1,9 +1,17 @@
 <?php
 
+// development
 $severname = "localhost";
 $username = "root";
 $password = "";
 $dbname = "unitime";
+
+// production
+// $severname = "localhost";
+// $username = "masjid29_altaf";
+// $password = "altafunitimemoprog";
+// $dbname = "masjid29_unitime";
+
 
 $conn = new mysqli($severname, $username, $password, $dbname);
 
--- tugas/add.php
+++ tugas/add.php
@@ -1,4 +1,9 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 include "../conn.php";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
