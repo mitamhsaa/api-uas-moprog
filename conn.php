08e412cac3a9fd65abbd3f986511a09d6a37f6c1
aftlah <altaf.fattah05@gmail.com>
2025-06-15 01:36:06 +0700

    add: prod altaf

--- conn.php
+++ conn.php
@@ -1,21 +1,27 @@
 <?php
 
 // development
-$severname = "localhost";
-$username = "root";
-$password = "";
-$dbname = "unitime";
+// $severname = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "unitime";
 
-// production
+// production dias
 // $severname = "localhost";
 // $username = "masjid29_altaf";
 // $password = "altafunitimemoprog";
 // $dbname = "masjid29_unitime";
 
+// production altaf
+$severname = "localhost";
+$username = "aftx9255_aftlah_unitime";
+$password = "aftlah_unitime";
+$dbname = "aftx9255_unitime";
+
 
 $conn = new mysqli($severname, $username, $password, $dbname);
 
-if ($conn->connect_error){
-    die("Koneksi gagal : ". $conn->connect_error);
+if ($conn->connect_error) {
+    die("Koneksi gagal : " . $conn->connect_error);
 }
 ?>
